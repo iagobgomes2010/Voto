632416fa23b9b5dfb46cf08bf8ecca353c422398
iagobgomes2010 <145400842+iagobgomes2010@users.noreply.github.com>
2025-05-26 17:44:05 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+import time
+
+form_url = 'https://docs.google.com/forms/d/e/1FAIpQLSeWXyIqcleTTfZj01oO_ZqdZ92AD96ldbAULxDRMjzawcxwyw/formResponse'
+
+data = {
+    'entry.474209580': 'Equipe mãos limpas, cuidado seguro (Setor: auditoria)'
+}
+
+while True:
+    response = requests.post(form_url, data=data)
+    if response.status_code == 200:
+        print('Resposta enviada com sucesso!')
+    else:
+        print(f'Erro ao enviar: {response.status_code}')
+    time.sleep(1)
